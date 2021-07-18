db4cad73a4ab3ddbfe989055ed6d3d9aafed225a
sho matsuda <qqq.ms1126@gmail.com>
2021-07-18 19:51:50 +0900

    Add full_title helper

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
-module ApplicationHelper
+module ApplicationHelper  # Railsでは自動的にヘルパーモジュールを読み込んでくれる=>includeをわざわざ書く必要がない。
+  # カスタムヘルパー
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')   # オプション引数('')も設定
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title                    # 暗黙の戻り値
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
