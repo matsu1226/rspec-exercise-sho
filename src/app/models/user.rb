0d51ae30bf4dcf8de2cc2b6575e01dcddb22cdef
sho matsuda <qqq.ms1126@gmail.com>
2021-07-30 22:37:44 +0900

    12.3.2(パスワードリセットに関するcontroller記述)まで終了

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # remember_token => 変数として定義し、get/setできるが、DBには保存しない(仮想の属性)。
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email # before_save => オブジェクト保存直前 = オブジェクトの作成時 + 更新時
   before_create :create_activation_digest   # before_create => オブジェクトが作成時のみ
@@ -61,6 +61,24 @@ class User < ApplicationRecord
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token - User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    # = user.reset_digest = User.digest(reset_token)
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
